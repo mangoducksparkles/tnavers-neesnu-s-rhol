83b712bc1257ed1fdc10e51d8267910bb6df6e45
mangoduck <75932022+mangoducksparkles@users.noreply.github.com>
2021-12-13 14:25:08 -0800

    fixed tabs in main.py

--- bot/main.py
+++ bot/main.py
@@ -6,12 +6,12 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+	print('We have logged in as {0.user}'.format(client))
 
 @bot.command()
 async def c(ctx, *, msg):
-    await ctx.send(str(msg).reverse(), tty=True)
+	await ctx.send(str(msg).reverse(), tty=True)
 	await ctx.message.delete()
 
 if __name__ == "__main__":
-    bot.run(TOKEN)
+	bot.run(TOKEN)
